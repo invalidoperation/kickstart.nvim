e5eb3b56cd8c4dda5995cb7ed33c3d66e6ad434b
Tobias Kappe <tobias@Tobiass-MacBook-Pro.local>
2025-05-24 23:30:22 +0200

    Add special configuration for LazyGit in Toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,5 +1,42 @@
 return {
-  { 'akinsho/toggleterm.nvim', version = '*', opts = {
-    open_mapping = [[<leader>tt]],
-  } },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<leader>tt]],
+    },
+    init = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
+        dir = 'git_dir',
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd 'startinsert!'
+          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd 'startinsert!'
+        end,
+      }
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+    end,
+    keys = {
+      {
+        '<leader>tg',
+        '<cmd>lua _lazygit_toggle()<CR>',
+        desc = 'Toggle LazyGit',
+        noremap = true,
+        silent = true,
+      },
+    },
+  },
 }
