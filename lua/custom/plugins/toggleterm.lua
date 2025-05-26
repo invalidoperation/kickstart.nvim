e25d6c99a27a7617939520c1da4e3bf976207784
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-05-26 11:26:28 +0200

    Update toggleterm configuration

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,10 +2,32 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = '*',
+    lazy = false,
     opts = {
       open_mapping = [[<leader>tt]],
+      direction = 'float',
+      float_opts = {
+        border = 'single',
+      },
     },
-    init = function()
+    config = function(_, opts)
+      if vim.fn.has 'win32' then
+        local powershell_options = {
+          shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+          shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+          shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+          shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+          shellquote = '',
+          shellxquote = '',
+        }
+
+        for option, value in pairs(powershell_options) do
+          vim.opt[option] = value
+        end
+      end
+
+      require('toggleterm').setup(opts)
+
       local Terminal = require('toggleterm.terminal').Terminal
       local lazygit = Terminal:new {
         cmd = 'lazygit',
