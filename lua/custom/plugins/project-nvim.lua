049282629fef7ccd4756470ca7cdfc335605217c
Tobias Kappe <t.kappe@keyence.de>
2025-06-03 14:09:31 +0200

    Change telescope configuration handling for project.nvim
    Set project.nvim as dependency of telescope
    Remove telescope dependency of project.nvim
    Configure keymap in telescope config instead of project.nvim

--- lua/custom/plugins/project-nvim.lua
+++ lua/custom/plugins/project-nvim.lua
@@ -1,19 +1,6 @@
 return {
   'ahmedkhalf/project.nvim',
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
-  },
-  config = function(_, opts)
-    require('project_nvim').setup(opts)
-    require('telescope').load_extension 'projects'
+  config = function()
+    require('project_nvim').setup {}
   end,
-  keys = {
-    {
-      '<leader>sp',
-      function()
-        require('telescope').extensions.projects.projects {}
-      end,
-      desc = '[S]earch Recent [P]rojects',
-    },
-  },
 }
