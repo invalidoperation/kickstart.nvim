35e9bf2178f63fe812adfd1c8a18e46637335c6e
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-06-07 20:54:02 +0200

    Change blink.cmp to 'enter' preset

--- init.lua
+++ init.lua
@@ -850,8 +850,19 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'enter',
 
+        ['<Tab>'] = {
+          'select_next',
+          'snippet_forward',
+          'fallback',
+        },
+
+        ['<S-Tab>'] = {
+          'select_prev',
+          'snippet_backward',
+          'fallback',
+        },
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
@@ -866,6 +877,11 @@ require('lazy').setup({
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        list = {
+          selection = {
+            preselect = false,
+          },
+        },
       },
 
       sources = {
