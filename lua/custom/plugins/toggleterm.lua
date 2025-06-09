ada6288fed096ebcd7e67f43c9f09be9977c84ea
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-06-10 00:24:21 +0200

    Fix OS check in toggleterm.lua and update Lazy modules

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -18,7 +18,7 @@ return {
       },
     },
     config = function(_, opts)
-      if vim.fn.has 'win32' then
+      if vim.fn.has 'win32' == 1 then
         local powershell_options = {
           shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
           shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
@@ -31,6 +31,8 @@ return {
         for option, value in pairs(powershell_options) do
           vim.opt[option] = value
         end
+      elseif vim.fn.has 'unix' == 1 then
+        vim.opt.shell = '/bin/zsh'
       end
 
       require('toggleterm').setup(opts)
