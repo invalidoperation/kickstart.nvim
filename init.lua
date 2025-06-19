9c0b562cb60532f32c0575b08b0909abef03bfec
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-06-19 14:30:29 +0200

    Enable expandtab and set tabstop & shiftwidth to 2

--- init.lua
+++ init.lua
@@ -152,6 +152,11 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- Expand tabs as spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
