1cf6fe70fdce228d0576555619c0e0e0a77c0b5b
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-07-24 10:06:02 +0200

    Make tokyonight colorscheme transparent

--- init.lua
+++ init.lua
@@ -974,7 +974,10 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
           comments = { italic = false }, -- Disable italics in comments
         },
       }
@@ -982,7 +985,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
