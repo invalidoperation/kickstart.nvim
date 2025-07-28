687132a117c6786fcc81332d6484b035ca4001c7
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-07-28 09:19:57 +0200

    Disable transparency in theme

--- init.lua
+++ init.lua
@@ -987,10 +987,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
-        transparent = true,
         styles = {
-          sidebars = 'transparent',
-          floats = 'transparent',
           comments = { italic = false }, -- Disable italics in comments
         },
       }
