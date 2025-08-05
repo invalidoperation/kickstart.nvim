b7678a82b0b9e73c9ce3589226e7a3aa6ff57c29
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-08-05 13:20:13 +0200

    Add keymap for lazygit.log_file()

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -12,5 +12,12 @@ return {
       end,
       desc = 'Lazygit',
     },
+    {
+      '<leader>gl',
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = 'Lazygit current file [l]og',
+    },
   },
 }
