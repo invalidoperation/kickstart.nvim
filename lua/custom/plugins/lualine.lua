f04b0698db9e828ce3c6266908dcf13cc1cbedac
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-08-16 14:38:36 +0200

    Configure lualine.nvim

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,9 +1,18 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons', 'AndreM222/copilot-lualine' },
-  opts = {
-    sections = {
-      lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
-    },
-  },
+  config = function()
+    local theme = require 'lualine.themes.auto'
+    theme.terminal = {
+      a = { fg = '#121715', bg = '#4bc997' },
+    }
+
+    require('lualine').setup {
+      options = { theme = theme },
+      extensions = { 'neo-tree', 'toggleterm', 'nvim-dap-ui' },
+      sections = {
+        lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
+      },
+    }
+  end,
 }
