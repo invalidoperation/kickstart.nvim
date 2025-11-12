a2d61afbf53dfaafdf182e578817f3651910e8b2
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-11-12 09:12:34 +0100

    Update project.nvim setup

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,6 +1,11 @@
 return {
   'DrKJeff16/project.nvim',
   config = function()
-    require('project').setup {}
+    require('project').setup {
+      show_hidden = true,
+      ignore_lsp = {
+        'easy_dotnet', -- causes issues due to root directory containing trailing '/' (at least on windows)
+      },
+    }
   end,
 }
