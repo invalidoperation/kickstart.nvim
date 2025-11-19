eaf926bfa15695f704142bbcf2727ac7bb5b552f
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-11-19 14:53:04 +0100

    Remove unused function

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,20 +1,3 @@
-local function rebuild_project(co, path)
-  local spinner = require('easy-dotnet.ui-modules.spinner').new()
-  spinner:start_spinner 'Building'
-  vim.fn.jobstart(string.format('dotnet build %s', path), {
-    on_exit = function(_, return_code)
-      if return_code == 0 then
-        spinner:stop_spinner 'Built successfully'
-      else
-        spinner:stop_spinner('Build failed with exit code ' .. return_code, vim.log.levels.ERROR)
-        error 'Build failed'
-      end
-      coroutine.resume(co)
-    end,
-  })
-  coroutine.yield()
-end
-
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
