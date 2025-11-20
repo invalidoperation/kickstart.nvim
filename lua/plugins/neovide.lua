d24f01d193da63389890ca81d5bd40ef882db2ce
Tobias Kappe <9612541+invalidoperation@users.noreply.github.com>
2025-11-20 15:42:13 +0100

    Enable subpixelantialias for guifont in neovide

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-  vim.o.guifont = 'JetBrainsMono Nerd Font:h10'
+  vim.o.guifont = 'JetBrainsMono Nerd Font:h10:#e-subpixelantialias'
   vim.g.neovide_scale_factor = 1.0
 
   vim.g.neovide_title_background_color = string.format('%x', vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name 'Normal' }).bg)
